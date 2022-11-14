3ee177af6d8727a63d4d51e49580c90c44cf823a
Lzccc <109495689+LinZC24@users.noreply.github.com>
2022-11-14 22:03:44 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -22,9 +22,66 @@ String operator""_ansi(const wchar_t* src, std::size_t len) {
     return result;
 }
 
+bool firstMouse = true;
+
+//定义数组存储棋盘状态
+char broad[9][9]{};
+
+/*定义按钮的状态
+struct buttonstate{
+  bool hovered;
+  bool pressed; 
+}*/
+
+//定义矩形按钮
+struct RectButton
+{
+	int x, y;
+	int width, height;
+};
+
+//判断点是否在按钮点击区域内
+bool insideRectButton(const RectButton* button, int x, int y);
+
+
+
+
+//定义按钮，确定区域
+RectButton button1 = {
+	100, 200,  /* x, y */
+	500, 100,   /* width, height */
+};
+RectButton button2 = {
+  100,350,
+  500,100
+};
+RectButton button3 = {
+  100,500,
+  500,100
+};
+RectButton button4 = {
+  100,650,
+  500,100
+};
+
+//绘制按钮
+void drawRectButton(const RectButton* button)
+{
+  setfillcolor(EGEARGB(0,0,0,0));
+  ege_rectangle(button->x, button->y, 
+                button->width, button->height);
+}
+
+//判断是否在按钮内部
+bool insideButton(const RectButton* button,int x,int y)
+{
+  return (x >= button->x) && (y >= button->y)
+      && (x <= button->x + button->width)
+      && (y <= button->y + button->height);
+}
+
 int main()
 {
-  
   //设置字体
 
   int broadWidth = 80,broadHeight = 80;
@@ -70,16 +127,45 @@ int main()
     -查看历史游戏
     -退出
   */
-  int bottomWidth = 160,bottomHeight = 100;
-  color_t bottom = EGEARGB(210,65,105,225);
-  setfillcolor(bottom);
+
+  //打印菜单
+  //在菜单文字下方打印透明的按钮实现点击功能
+  setfont(75,0,"微软雅黑");
+  xyprintf(420,100,L"开始游戏"_ansi);
+
+  int buttonWidth = 20,buttonHeight = 100;
+  color_t button = EGEARGB(210,65,105,225);
   
-  ege_fillrect(340,200,bottomWidth,bottomHeight);
-  ege_fillrect(340,350,bottomWidth,bottomHeight);
-  ege_fillrect(340,500,bottomWidth,bottomHeight);
+  
+  setfont(35,0,"宋体");
+  setbkmode(TRANSPARENT);
+
+  //设置文字对齐方式为水平左对齐，垂直中心对齐
+  settextjustify(LEFT_TEXT, CENTER_TEXT);
+
+  setfillcolor(button);
+  ege_fillrect(100,200,buttonWidth,buttonHeight);
+  xyprintf(135,250,L"开始游戏(S)"_ansi);
+  drawRectButton(&button1);
+
+  setfillcolor(button);
+  ege_fillrect(100,350,buttonWidth,buttonHeight);
+  xyprintf(135,400,L"继续游戏(C)"_ansi);
+  drawRectButton(&button2);
+
+  setfillcolor(button);
+  ege_fillrect(100,500,buttonWidth,buttonHeight);
+  xyprintf(135,550,L"查看历史游戏(H)"_ansi);
+  drawRectButton(&button3);
+
+  setfillcolor(button);
+  ege_fillrect(100,650,buttonWidth,buttonHeight);
+  xyprintf(135,700,L"退出(ESC)"_ansi);
+  drawRectButton(&button4);
 
   //在这中间写处理鼠标命令的代码
-  getch();//按任意键继续
+  getch();
+  //按任意键继续
 
   cleardevice();
   
@@ -87,7 +173,7 @@ int main()
   //从这里开始绘制棋盘（大小为9*9）（以及UI）
   color_t brown = EGEARGB(223,249,214,91);
 
-  for(int row{};row < nRow;row++){
+  /*for(int row{};row < nRow;row++){
     for(int col{};col < nCol;col++){
       int x = col * broadWidth + 100, y = row * broadHeight + 100;
       color_t broadColor = brown;
@@ -95,7 +181,17 @@ int main()
       setfillcolor(broadColor);
       ege_fillrect(x, y, broadWidth, broadHeight);
     }
-  }
+  }*/
+  setfont(20,0,"微软雅黑");
+  setfillcolor(button);
+  ege_fillrect(10,10,50,25);
+  xyprintf(10,17,L"暂停"_ansi);
+
+  color_t broadColor = brown;
+
+  setfillcolor(broadColor);
+  ege_fillrect(50,50,740,740);
+
   for(int row{};row < nRow;row++){
     for(int col{};col < nCol;col++){
       int x = col * broadWidth + 100, y = row * broadHeight + 100;
@@ -105,7 +201,7 @@ int main()
       ege_rectangle(x, y, broadWidth, broadHeight);
     }
   }
-
+  //游戏结束，关闭窗口
   getch();
 
   closegraph();
